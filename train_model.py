23887910d2c2a89b7e938c52cab794f47d652d85
Brennen Awana <brennen.awana@gmail.com>
2024-05-28 21:15:36 +0000

    train basic model

---
+++ train_model.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+import joblib
+
+data = load_iris()
+X, y = data.data, data.target
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, 'model.pkl')
